0a384f9ca5f67fc1c7431c17a71991be0ef7dc35
Raúl Castañón <borchinche@hotmail.com>
2025-03-09 01:37:54 -0600

    Crea función para crear amigos, validarlos y limpiar campo de entrada

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Array para almacenar nombres de los amigos
+let listaAmigos = [];
+
+//Función para agregar nombre de amigos
+function agregarAmigo() {
+    const input = document.getElementById('amigo');
+    const amigo = input.value.trim();
+//Validar entrada
+    if (nombre === '') {
+        alert ('Por favor, inserte un nombre.');
+        return;
+    }
+//Actualizar array
+    listaAmigos.push(nombre);
+//Limpiar campo
+    input.value = '';
+//Verificar
+    console.log('Lista de amigos:', listaAmigos);
+}
